2f9124fe9d6c329c44a9e8728f3bc877cb5b4b2d
hanzelsabio <zeluckgt01@gmail.com>
2026-03-20 06:05:39 +0800

    Revert "feat: update API base URL for development and production"

--- frontend/src/admin/modules/pms/categories/services/categoryAPI.js
+++ frontend/src/admin/modules/pms/categories/services/categoryAPI.js
@@ -4,7 +4,7 @@ export const API_BASE_URL =
   import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${API_BASE_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
